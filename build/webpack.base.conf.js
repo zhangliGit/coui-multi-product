c65428fc7900cdb6fc277a125fb077e9687dfcb0
zhangli <zhangli>
2018-08-07 11:08:03 +0800

    新增配置 编译npm引用的模块

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -50,7 +50,7 @@ let webpackConfig = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('node_modules/corlib'), resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
