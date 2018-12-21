8c8b7c7d050588f3da2b2db260741b798e1d595c
zhangli <zhangli>
2018-12-21 15:24:13 +0800

    add component

---
+++ src/pages/demo-page/views/form-com/Radio.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="alert co-f1 co-flex co-ver">
+    <header-com isBack :title="title"></header-com>
+    <div class="co-f1">
+      <div class="co-flex co-ac co-bg-0 co-bd-b co-pd-a05">
+        <div class="">事故类型:</div>
+        <div class="co-f1 co-flex co-je">
+          <radio v-model="selectList" is-disable :list-item="list"></radio>
+        </div>
+      </div>
+      <div class="co-flex co-ac co-bg-0 co-bd-b co-pd-a05">
+        <div class="">事故类型1:</div>
+        <div class="co-f1 co-flex co-je">
+          <radio v-model="selectList1" :list-item="list" is-icon is-margin is-circle></radio>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderCom from '@c/HeaderCom'
+import Radio from '@c/Radio'
+export default {
+  name: 'RadioCom',
+  components: {
+    HeaderCom,
+    Radio
+  },
+  data () {
+    return {
+      title: '单选框',
+      list: [
+        {
+          id: 0,
+          name: '一般'
+        },
+        {
+          id: 1,
+          name: '普通'
+        },
+        {
+          id: 2,
+          name: '特大'
+        },
+      ],
+      selectList: [
+        {
+          id: 0,
+          name: '一般'
+        }
+      ],
+      selectList1: []
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  /deep/ .co-radio {
+    .co-radio-list--active {
+      background-color: blue !important;
+      color: #fff
+    }
+  }
+</style>
---
+++ src/pages/demo-page/views/project-com/ClockTime.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="time-line co-f1 co-flex co-ver">
+    <header-com isBack :title="title"></header-com>
+    <div class="co-f1 co-flex">
+      <scroll-list>
+        <clock-time :time-list = "timeList"></clock-time>
+      </scroll-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderCom from '@c/HeaderCom'
+import ScrollList from '@c/ScrollList'
+import ClockTime from '@c/ClockTime'
+export default {
+  name: 'TimeLineCom',
+  components: {
+    HeaderCom,
+    ScrollList,
+    ClockTime
+  },
+  data () {
+    return {
+      title: '打卡时间轴',
+      timeList: [
+        {
+          id: 0,
+          time: '2018-08-29',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 0,
+          tag: '0'
+        },
+        {
+          id: 1,
+          time: '2018-08-28',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 0,
+          tag: '2'
+        },
+        {
+          id: 2,
+          time: '2018-08-27',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 0,
+          tag: '1'
+        },
+        {
+          id: 3,
+          time: '2018-08-26',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 1,
+          tag: '2'
+        },
+        {
+          id: 0,
+          time: '2018-08-29',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 1,
+          tag: '0'
+        },
+        {
+          id: 1,
+          time: '2018-08-28',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 1,
+          tag: '2'
+        },
+        {
+          id: 2,
+          time: '2018-08-27',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 1,
+          tag: '1'
+        },
+        {
+          id: 3,
+          time: '2018-08-26',
+          address: '武汉市洪山区花山大道花山软件新城一期A2栋',
+          tip: 1,
+          tag: '2'
+        }
+      ]
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang = "less">
+</style>
